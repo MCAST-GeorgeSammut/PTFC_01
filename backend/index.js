e319c82f830fd1756ed23ea75accd59e8fb3627d
MCAST-GeorgeSammut <george.sammut.c10331@mcast.edu.mt>
2022-02-18 10:31:14 +0100

    Added token based authentication for backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import Express from "express";
 import cors from "cors";
+import { v4 as uuid } from 'uuid';
 
 //Don't forget to put "type": "module" under "main" in package.json
 
@@ -7,22 +8,39 @@ import cors from "cors";
 const app = Express();
 //port number that connections will go through
 const PORT = 3001;
+const secretToken = uuid();
+let requests = 0;
+
+
+app.get("/secret",(req,res) =>{
+    const token = req.query.token;
+    requests++;
+    if(token == secretToken){
+        res.send({result: "200", requests : requests, message:"This is a very secret message"});
+    }else{
+        res.send({result: "401", message:"Invalid token."});
+    }
+});
+
+
+
+// app.get("/hello",(req,res) =>{
+//     const username = req.query.username;
+//     const password = req.query.password;
+//     if(username == "Joe" && password == "123"){
+//         res.send("Hello Joe!");
+//     }else{
+//         res.send("Invalid credentials.");
+//     }
+
+//     //console.log(username + " " + password);
+//     res.send("Hello world!");
+// });
+
 
 // allowing express to use cors to bypass cross oriign requests
 app.use(cors());
 
-app.get("/hello",(req,res) =>{
-    const username = req.query.username;
-    const password = req.query.password;
-    if(username == "Joe" && password == "123"){
-        res.send("Hello Joe!");
-    }else{
-        res.send("Invalid credentials.");
-    }
-
-    //console.log(username + " " + password);
-    res.send("Hello world!");
-});
 
 
 //telling the server to start listening
